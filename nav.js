8aff3c8e77edd46e798002fbb390b70c14e8a950
PlainApple2008 <danielderrickmcalisura@gmail.com>
2025-10-25 11:07:05 +0800

    init files

---
+++ nav.js
@@ -0,0 +1,46 @@
+// Global Data
+const pageLinkDisplay = ["Home", "About Me"];
+const pageAnchors = ["/index.html", "/about.html"];
+
+function getCurrentFilePath() {
+    return window.location.pathname;
+}
+
+function createNavigation() {
+    const header = document.getElementsByTagName("header")[0];
+    
+    const nav = document.createElement("nav");
+    
+    const ul = document.createElement("ul");
+    
+    for (let i = 0; i < pageAnchors.length; i++) {
+        let li = document.createElement("li");
+        
+        let a = document.createElement("a");
+        
+        let userInCurrentPage = pageAnchors[i] === getCurrentFilePath();
+        
+        if (userInCurrentPage) {
+            a.classList.add("active-page");
+        } else {
+            a.setAttribute("href", pageAnchors[i]);
+        }
+        
+        a.innerHTML = pageLinkDisplay[i];
+        
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+    
+    nav.appendChild(ul);
+    header.appendChild(nav);
+    // console.log(header);
+}
+
+function insertFooterContent() {
+    let footer = document.getElementsByTagName("footer")[0];
+    footer.innerHTML = "&copy;2026 Daniel Derrick Calisura";
+}
+
+createNavigation();
+insertFooterContent();
---
+++ pagination.js
@@ -0,0 +1,9 @@
+let pageAnchors = [
+    "/disciplines/programming.html",
+    "/disciplines/music.html",
+    "/disciplines/digital_art.html"
+];
+
+let main = document.getElementsByTagName("main");
+
+let 
